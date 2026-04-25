88a166364cb111f32dce3c72d9ef50c457405d52
AndrewTheCoder <andrew.blackwell23@yahoo.com>
2026-04-25 12:04:07 -0500

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function MainPage() {
+  return (
+    <main>
+      <h1>Welcome to the Main Page</h1>
+      <p>This is the content of the main page.</p>
+      
+    </main>
+  )
+}
